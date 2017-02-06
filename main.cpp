3bae8621b8291b932c269f0286a5f357d491079f
Mitch <Mitch@Mitch-PC.home>
2017-02-06 15:30:59 +0000

    Added 'interlocks' and auto shutoff when dial is mostly off

--- main.cpp
+++ main.cpp
@@ -47,8 +47,8 @@ static int measureSensor() // This measures both ADC channels and saves their va
 	a2dVal = (data[1] << 8) & 0b1100000000; //merge data[1] & data[2] to get result
 	a2dVal |= (data[2] & 0xff);
 	FWD_val = a2dVal;
-	
-	
+
+
 	data[0] = 1; //  first byte transmitted -> start bit
 	data[1] = 0b10000000 | (((A2DCHANNELREV & 7) << 4)); // second byte transmitted -> (SGL/DIF = 1, D2=D1=D0=0)
 	data[2] = 0; // third byte transmitted....don't care
@@ -66,57 +66,60 @@ static int measureSensor() // This measures both ADC channels and saves their va
 
 static void motorForward()
 {
-	bcm2835_pwm_set_data(REV_PWM_CHANNEL, 0);
+	bcm2835_pwm_set_mode(REV_PWM_CHANNEL, 0, 0);
 	while (FWD_val >= 30)
 	{
 		measureSensor();
 		bcm2835_pwm_set_data(FWD_PWM_CHANNEL, FWD_val);
-		cout << "Forward channel set to: " << FWD_val << endl;
-		delay(100);
+		//		cout << "Forward channel set to: " << FWD_val << endl;
+		//		delay(100);
 	}
 	bcm2835_pwm_set_data(FWD_PWM_CHANNEL, 0);
+	bcm2835_pwm_set_mode(REV_PWM_CHANNEL, 1, 1);
 }
 
 static void motorReverse()
 {
-		bcm2835_pwm_set_data(FWD_PWM_CHANNEL, 0);
+	bcm2835_pwm_set_mode(FWD_PWM_CHANNEL, 0, 0);
 	while (REV_val >= 30)
 	{
 		measureSensor();
 		bcm2835_pwm_set_data(REV_PWM_CHANNEL, REV_val);
-		cout << "Reverse channel set to: " << REV_val << endl;
-		delay(100);
+		//		cout << "Reverse channel set to: " << REV_val << endl;
+		//		delay(100);
 	}
 	bcm2835_pwm_set_data(REV_PWM_CHANNEL, 0);
+	bcm2835_pwm_set_mode(FWD_PWM_CHANNEL, 1, 1);
 }
 
 int main()
 {
 	if (!bcm2835_init())
 		return 1;
-//--------------SETUP HERE----------------//
+	//--------------SETUP HERE----------------//
 	bcm2835_gpio_fsel(MOTORFORWARDPIN, BCM2835_GPIO_FSEL_ALT0); //Make forward and reverse pins connected to respective PWM clocks.
 	bcm2835_gpio_fsel(MOTORREVERSEPIN, BCM2835_GPIO_FSEL_ALT5);
 	bcm2835_pwm_set_clock(BCM2835_PWM_CLOCK_DIVIDER_512); //Set clock divider.
-	
+
 	bcm2835_pwm_set_mode(FWD_PWM_CHANNEL, 1, 1); // Set mode and range for both forward and reverse channels
 	bcm2835_pwm_set_range(FWD_PWM_CHANNEL, RANGE);
-	
+
 	bcm2835_pwm_set_mode(REV_PWM_CHANNEL, 1, 1);
 	bcm2835_pwm_set_range(REV_PWM_CHANNEL, RANGE);
-	
-//---------------START OF MAIN LOOP---------//
+
+	//---------------START OF MAIN LOOP---------//
 	int i = 0;
 	while (true)
 	{
+		bcm2835_pwm_set_data(REV_PWM_CHANNEL, 0);
+		bcm2835_pwm_set_data(FWD_PWM_CHANNEL, 0);
 		measureSensor();
-		//bcm2835_pwm_set_data(PWM_CHANNEL, a2dVal);
-//		if (FWD_val >= 30)
-//			motorForward();
-//		else if (REV_val >= 30)
-//			motorReverse();
-		bcm2835_pwm_set_data(0, REV_val);
-		bcm2835_pwm_set_data(1, FWD_val);
+				if (FWD_val >= 30)
+					motorForward();
+				else if (REV_val >= 30)
+					motorReverse();
+//		bcm2835_pwm_set_data(0, REV_val);
+//		bcm2835_pwm_set_data(1, FWD_val);
 		if (i % 10000 == 0)
 		{
 			cout << "The sensor values are; Forward: " << FWD_val << " | Reverse: " << REV_val << endl;
